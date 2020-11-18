33d2076e6217edb1da855719ffedf1afa6827eeb
Kapitan Rebrow <36919372+DominikFerenc@users.noreply.github.com>
2020-11-18 11:43:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,5 @@
-#include <iostream>
-#include <fstream>
-#include <linux/watchdog.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/ioctl.h>
-#include <fcntl.h>
-#include <getopt.h>
-#include <string.h>
-#include <errno.h>
-#include <sys/io.h>
-#include <unistd.h>
 #include <watchdogioctl.h>
 
-
-
-//using namespace std;
 
 int main()
 {
